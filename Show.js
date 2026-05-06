26720a08f0b25ce98544a8cfcee41f4d4f6a396a
starsimon32 <starsimon32@gmail.com>
2026-05-06 16:49:11 +0800

    Update Show.js

--- Show.js
+++ Show.js
@@ -1,12 +1,24 @@
-export function showNumber(input,select,doubles,MD,Gresult,isChecked) {
-  let text = select.options[select.selectedIndex].text;
+export function showNumber(input,suit,doubles,MD,Gresult,isChecked) {
+  let suitText = {
+
+    a: "club",
+
+    b: "diamond",
+
+    c: "heart",
+
+    d: "spade",
+
+    e: "NT"
+
+  }[suit]
     let text2="結果：";
     if (isChecked) {
         text2="有身價";
     } else {
        text2="沒身價";
     }
-  text2+=input+text;
+  text2+=input+suitText;
   for(let i=1;i<=doubles;i++){
     text2+="X";
   }
