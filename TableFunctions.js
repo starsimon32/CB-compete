3f2de212e22222de33bfea18293b7f0dd35fd4a8
starsimon32 <starsimon32@gmail.com>
2026-05-07 15:19:06 +0800

    Update TableFunctions.js

--- TableFunctions.js
+++ TableFunctions.js
@@ -13,9 +13,3 @@ export function setTableCell(tableId, row, col, text) {
   let table = document.getElementById(tableId);
   table.rows[row].cells[col].innerText = text;
 }
-export function initialTable(rounds, tableId) {
-  let table = document.getElementById(tableId);
-  setTableCell(table,1,1,"圈"）；
-  setTableCell(table,1,2,"身價"）；
-    setTableCell(table,1,3,"圈"）；
-}
