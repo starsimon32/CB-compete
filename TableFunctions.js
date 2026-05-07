8645168ef7c65957e82aedf1f50e4941e5ed1f58
starsimon32 <starsimon32@gmail.com>
2026-05-07 22:44:30 +0800

    Update TableFunctions.js

--- TableFunctions.js
+++ TableFunctions.js
@@ -7,9 +7,20 @@
       counttotalpoints
     } from './score.js';
 import {
-      getVul
+      getVul,
+    getDealer
     } from './GetVul.js';
 export function setTableCell(tableId, row, col, text) {
   let table = document.getElementById(tableId);
   table.rows[row].cells[col].innerText = text;
+}
+export function setblanktable(tableID,rounds,start){
+    let table =tableID;
+    for(let i=1;i<=rounds;i++){
+  let row = table.insertRow();
+        let a=i+start-1;
+      row.insertCell(0).innerText = (a);
+  row.insertCell(1).innerText = getDealer(a);
+     row.insertCell(2).innerText = getVul(a);   
+    }
 }
