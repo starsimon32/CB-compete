d4f531311b2f5056dd60180a54b459d5a946d250
starsimon32 <starsimon32@gmail.com>
2026-05-07 14:36:23 +0800

    Update TableFunctions.js

--- TableFunctions.js
+++ TableFunctions.js
@@ -9,26 +9,13 @@
 import {
       getVul
     } from './GetVul.js';
+export function setTableCell(tableId, row, col, text) {
+  let table = document.getElementById(tableId);
+  table.rows[row].cells[col].innerText = text;
+}
 export function initialTable(rounds, tableId) {
   let table = document.getElementById(tableId);
-  table.innerHTML = "";
-  let header1 = table.insertRow();
-  header1.insertCell(0).innerText = "牌";
-  header1.insertCell(1).innerText = "身價";
-  header1.insertCell(2).innerText = "我們桌";
-  header1.insertCell(3).innerText = "他們桌";
-  header1.insertCell(4).innerText = "我們IMP";
-  header1.insertCell(5).innerText = "他們IMP";
-  header1.insertCell(6).innerText = "差值";
-  for (let i = 1; i <= rounds; i++) {
-    let row = table.insertRow();
-    row.insertCell(0).innerText = i;
-    let vul = getVul(i);
-    row.insertCell(1).innerText = vul;
-    row.insertCell(2).innerText = "";
-    row.insertCell(3).innerText = "";
-    row.insertCell(4).innerText = "";
-    row.insertCell(5).innerText = "";
-    row.insertCell(6).innerText = "";
-  }
+  setTableCell(table,1,1,"圈"）；
+  setTableCell(table,1,2,"身價"）；
+    setTableCell(table,1,3,"圈"）；
 }
